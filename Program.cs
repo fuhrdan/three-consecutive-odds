e9a09e8baf13dc565d3283d57d40b94fd84fc519
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-30 18:41:22 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//*****************************************************************************
+//** 1550. Three Consecutive Odds leetcode                                   **
+//** Simple Loop with modulo to solve -Dan                                   **
+//*****************************************************************************
+
+bool threeConsecutiveOdds(int* arr, int arrSize) {
+    for (int i = 0; i < arrSize-2; i++)
+    {
+        if((arr[i] % 2 == 1)&&(arr[i+1]%2==1)&&(arr[i+2]%2==1)) { return true;}
+//                printf("arr[%d] = %d, arr[%d] = %d, arr[%d] = %d\n",i,arr[i],i+1,arr[i+1],i+2,arr[i+2]);
+    }
+    return false;
+}
